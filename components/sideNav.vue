ce133de815673923e69435339b611aa5f3f6f6bf
Brian-mula <mulatiwangilabrian@gmail.com>
2022-07-12 23:10:08 +0300

    added purchase invoice page

--- components/sideNav.vue
+++ components/sideNav.vue
@@ -105,6 +105,50 @@
           </Popover>
         </li>
         <li>
+          <Popover class="relative">
+            <PopoverButton as="div" 
+              class="pl-10 flex items-center p-2 cursor-pointer text-base font-normal text-gray-50 hover:bg-blue-700 border-none "
+              >Purchases</PopoverButton
+            >
+            <transition
+              enter-active-class="transition duration-200 ease-out"
+              enter-from-class="translate-y-1 opacity-0"
+              enter-to-class="translate-y-0 opacity-100"
+              leave-active-class="transition duration-150 ease-in"
+              leave-from-class="translate-y-0 opacity-100"
+              leave-to-class="translate-y-1 opacity-0"
+            >
+              <div class="bg-white">
+                <PopoverPanel class="absolute z-10 bg-white w-full p-4 rounded">
+                  <div class="grid grid-cols-1">
+                    <nuxt-link
+                      to="/purchaseInvoice"
+                      class="flex items-center p-2 text-base font-normal text-gray-600 hover:bg-gray-100"
+                    >
+                      Purchase invoices
+                    </nuxt-link>
+                    <nuxt-link
+                      to="recurrent"
+                      class="flex items-center p-2 text-base font-normal text-gray-600 hover:bg-gray-100"
+                    >
+                      Purchase refunds
+                    </nuxt-link>
+                     <nuxt-link
+                      to="recurrent"
+                      class="flex items-center p-2 text-base font-normal text-gray-600 hover:bg-gray-100"
+                    >
+                      Manage suppliers
+                    </nuxt-link>
+                    
+                  </div>
+
+                  <img src="/solutions.jpg" alt="" />
+                </PopoverPanel>
+              </div>
+            </transition>
+          </Popover>
+        </li>
+        <li>
           
         </li>
       </ul>
