8916185eed0623051cc9c404ac9021b106b1e345
Brian-mula <mulatiwangilabrian@gmail.com>
2022-07-13 18:34:14 +0300

    modified expense page

--- pages/expenses.vue
+++ pages/expenses.vue
@@ -1,15 +1,58 @@
 <template>
-    <div>
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <h3 class="pt-2 pb-4 px-4 text-lg text-gray-500">Search</h3>
+  <div>
+    <div class="flex justify-between items-center mb-4">
+      <div class="flex justify-center w-80 cursor-pointer">
+        <div class="flex justify-center p-6 rounded-md shadow-lg bg-white w-full">
+          <div>
+          <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">
+            Last 7 days
+          </h5>
+          <p class="text-gray-700 text-base mb-4">
+            Ksh. 3000000
+          </p>
+          </div>
+         
+        </div>
+      </div>
+
+      <div class="flex justify-center w-80 cursor-pointer">
+        <div class=" p-6 rounded-md shadow-lg bg-white w-full flex justify-center">
+          <div>
+          <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">
+            Last 30 days
+          </h5>
+          <p class="text-gray-700 text-base mb-4">
+            Ksh. 3000000
+          </p>
+          </div>
+         
+        </div>
+      </div>
+
+      <div class="flex justify-center w-80 cursor-pointer">
+        <div class="flex justify-center p-6 rounded-md shadow-lg bg-white w-full">
+          <div>
+          <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">
+            Last year
+          </h5>
+          <p class="text-gray-700 text-base mb-4">
+            Ksh. 3000000
+          </p>
+          </div>
+         
+        </div>
+      </div>
+    </div>
+    <form class="bg-white shadow-md rounded px-8 pt-4 pb-8 mb-4">
+      <h3 class="pt-2 pb-2 px-4 text-lg text-gray-500">Search</h3>
       <div class="flex items-center justify-between">
         <div class="w-80 mx-4">
           <div class="mb-4 w-full">
             <label class="block text-gray-700 text-sm mb-2" for="username">
               Code:
             </label>
-             <input
-              class=" appearance-none border  w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            <input
+              class="appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               placeholder=""
             />
           </div>
@@ -20,39 +63,65 @@
               Date.
             </label>
             <input
-            type="date"
-              class=" appearance-none border  w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="date"
+              class="appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               placeholder=""
             />
           </div>
         </div>
-        
-      <div class="mb-4 w-80 mx-4">
-            <label class="block text-gray-700 text-sm mb-2" for="username">
-              Category.
-            </label>
-            <input
-              class=" appearance-none border  w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder=""
-            />
-          </div>
+
+        <div class="mb-4 w-80 mx-4">
+          <label class="block text-gray-700 text-sm mb-2" for="username">
+            Category.
+          </label>
+          <input
+            class="appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder=""
+          />
+        </div>
       </div>
       <div class="flex justify-end items-center">
-      <div class="mx-4">
-          <button type="button" class="inline-block px-6 py-2.5 bg-yellow-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out">Reset</button>
-      </div>
-       <div class="mx-4">
-        <button type="button" class="inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out">Search</button>
-       </div>
+        <div class="mx-4">
+          <button
+            type="button"
+            class="inline-block px-6 py-2.5 bg-yellow-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out"
+          >
+            Reset
+          </button>
+        </div>
+        <div class="mx-4">
+          <button
+            type="button"
+            class="inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out"
+          >
+            Search
+          </button>
+        </div>
       </div>
     </form>
+     <h3 class="bg-white text-gray-500 text-sm p-4 mt-4">Results</h3>
+      <div class="mt-8 mb-4">
+      <div
+        class="flex justify-between items-center border-b-4 bg-white rounded px-8 pt-3 pb-4"
+      >
+        <div>
+          <p class="font-normal text-gray-500 text-sm">#00001-8/7/2022</p>
+          
+        </div>
+       
+        <div>
+          <h5 class="mb-1 mt-1 text-lg tracking-tight text-gray-700">
+            Ksh.30000
+          </h5>
+          <div class="bg-red-800 flex justify-center items-center rounded-md">
+            <p class="font-normal text-gray-50 text-sm">Unpaid</p>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
