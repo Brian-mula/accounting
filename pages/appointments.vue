841d5e967ce4aba49c31a3a8a7a3878d3e71974b
Brian-mula <mulatiwangilabrian@gmail.com>
2022-07-23 00:18:28 +0300

    added new appointment page

--- pages/appointments.vue
+++ pages/appointments.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+  <div class="flex justify-end items-end p-3">
+      <nuxt-link to="/new_appointment"
+        class="bg-blue-600 p-2 rounded-sm cursor-pointer hover:bg-blue-700 flex items-center"
+      >
+        <img
+          src="~/assets/images/add-svgrepo-com.svg"
+          alt=""
+          class="h-5 w-5 mx-2"
+        />
+        <h4 class="text-white">New Appointment</h4>
+      </nuxt-link>
+    </div>
     <form class="bg-white shadow-md rounded px-8 pt-3 pb-4 mb-2">
       <h3 class="pt-2 pb-1 px-4 text-lg text-gray-500 border-b-2">Search</h3>
       <div class="grid grid-cols-3 gap-6 mt-2">
